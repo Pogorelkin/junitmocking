f574537cc5a5ee0d15374dfadf53295537e409a7
Vadim <warfirestb@mail.ru>
2019-07-10 04:58:42 +0400

    add unit test for business logic

--- src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -1,22 +1,47 @@
 package edu.epam.izhevsk.junit;
 
-import org.hamcrest.CustomMatcher;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import static org.mockito.AdditionalMatchers.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-
+import org.mockito.runners.MockitoJUnitRunner;
 import static org.mockito.Mockito.*;
 
-
-
-
+@RunWith(MockitoJUnitRunner.class)
 public class PaymentControllerTest {
 
- 
+    @Mock
+    AccountService mockAccountService;
+
+    @Mock
+    DepositService mockDepositService;
+
+    @InjectMocks
+    PaymentController paymentController = new PaymentController(mockAccountService, mockDepositService);
+
+    @Before
+    public void configureMocks() throws InsufficientFundsException {
+        doReturn(true).when(mockAccountService).isUserAuthenticated(100L);
+        doReturn(false).when(mockAccountService).isUserAuthenticated(not(eq(100L)));
+        doReturn("Successful").when(mockDepositService).deposit(lt(100L), anyLong());
+        doThrow(InsufficientFundsException.class).when(mockDepositService).deposit(geq(100L), anyLong());
+    }
+
+    @Test
+    public void testDepositAmount50UserId100AuthOnceWithUserId100() throws InsufficientFundsException {
+        paymentController.deposit(50L, 100L);
+        verify(mockAccountService, times(1)).isUserAuthenticated(100L);
+    }
+
+    @Test(expected = SecurityException.class)
+    public void testDepositForUnauthUser_SecurityExceptionExpected() throws InsufficientFundsException {
+        paymentController.deposit(50L, 1L);
+    }
+
+    @Test(expected = InsufficientFundsException.class)
+    public void testDepositOfLargeAmount_InsFundsExceptionExpected() throws InsufficientFundsException {
+        paymentController.deposit(1000L, 100L);
+    }
 }
